1567ababe44eff29771be45c026ac5b7985dfcdd
Scafariu Razvan <41546107+dragonten112@users.noreply.github.com>
2022-05-05 13:50:12 +0300

    Add files via upload

---
+++ Lab10/dist/script.dev.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function setCookie(cname, cvalue, sec) {
+  var d = new Date();
+  d.setTime(d.getTime() + sec * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+
+  return "";
+}
+
+function checkCookie() {
+  var user = getCookie("username");
+
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+
+setCookie("cheie1", "bunas", 30);
+setCookie("cheie2", "bBb", 30);
+getCookie("aAa", "3", 1000);
+//# sourceMappingURL=script.dev.js.map
---
+++ Lab10/script.js
@@ -0,0 +1,39 @@
+function setCookie(cname, cvalue, sec) {
+    const d = new Date();
+    d.setTime(d.getTime() + (sec * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
+
+  setCookie("cheie1","bunas",30);
+  setCookie("cheie2","bBb",30);
+  getCookie("aAa","3",1000);
+
+
